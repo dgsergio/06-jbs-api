2a1da38aae91a5dcbf7d3f9c5cc80b1d8ccc0a53
Sergio Garofalo <dg.sergio@gmail.com>
2023-07-26 12:58:47 +0200

    feat: mongoose errors customized

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -5,9 +5,29 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ msg: 'Something went wrong -.-' });
+  let respondErrors = {
+    msg: 'Something went wrong.',
+    code: StatusCodes.INTERNAL_SERVER_ERROR,
+  };
+
+  if (err.name === 'ValidationError') {
+    const errors = Object.values(err.errors);
+    const errorsMessages = errors.map((item) => item.message).join(', ');
+    respondErrors.code = StatusCodes.BAD_REQUEST;
+    respondErrors.msg = `${respondErrors.msg} ${errorsMessages}.`;
+  }
+
+  if (err.name === 'CastError') {
+    respondErrors.code = StatusCodes.NOT_FOUND;
+    respondErrors.msg = `${respondErrors.msg} No item found with id: ${err.value}.`;
+  }
+
+  if (err.code && err.code === 11000) {
+    respondErrors.code = StatusCodes.BAD_REQUEST;
+    respondErrors.msg = `${respondErrors.msg} ${err.keyValue.email} alredy exist in the DB. Do you want to login insted?`;
+  }
+
+  return res.status(respondErrors.code).json({ msg: respondErrors.msg });
 };
 
 module.exports = errorHandler;
